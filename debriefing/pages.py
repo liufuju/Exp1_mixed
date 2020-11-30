aad5ebc40530a220774a7ea974085df2217867b4
Horace <horace_liu@outlook.com>
2020-11-30 14:39:52 +0800

    futureV3.2

--- debriefing/pages.py
+++ debriefing/pages.py
@@ -4,6 +4,12 @@ from .models import Constants
 
 
 class Debriefing(Page):
+    def vars_for_template(self):
+        exp_theme = '回忆' if self.session.config['mode'] == 0 else '想象'
+        return dict(
+            theme=exp_theme
+        )
+
     form_model = 'player'
     form_fields = ['difficulty_debrief', 'purpose_debrief', 'imagination_diff', 'disturbance']
 
